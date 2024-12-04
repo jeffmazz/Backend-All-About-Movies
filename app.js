ec6426e492605e160a25fd3f14ce89da0aa55704
jeffmazz <jeff.mazzotta@hotmail.com>
2024-12-03 23:09:21 -0300

    Refact: Refatorando código para enviar apenas os dados necessários

--- app.js
+++ app.js
@@ -28,7 +28,35 @@ const fetchData = async(url, res) => {
         const response = await fetch(url, options)
         if(!response.ok) throw new Error("Erro na requisição à API do TMDB")
         const data = await response.json()
-        res.json(data)
+        const originalArray = data.results
+
+        const filteredArray = originalArray.map(item => ({
+            backdrop_path: item.backdrop_path,
+            poster_path: item.poster_path,
+            profile_path: item.profile_path,
+            title: item.title,
+            name: item.name,
+            original_title: item.original_title,
+            original_name: item.original_name,
+            overview: item.overview,
+            vote_average: item.vote_average,
+            vote_count: item.vote_count,
+            release_date: item.release_date,
+            first_air_date: item.first_air_date,
+            known_for: item.known_for,
+            popularity: item.popularity,
+            profile_path: item.profile_path,
+            name: item.name,
+            known_for: item.known_for.map(item => ({
+                title: item.title,
+                original_title: item.original_title,
+                vote_average: item.vote_average
+            })),
+            popularity: item.popularity
+        }))
+
+        res.json(filteredArray)
+
     } catch(error) {
         console.error("Error:", error)
         return res.status(500).json({error: error.message})
@@ -60,8 +88,28 @@ app.get('/series/popular', async(req, res) => {
 })
 
 app.get('/actors', async(req, res) => {
-    const url = 'https://api.themoviedb.org/3/person/popular?language=en-US&page=1'
-    await fetchData(url, res)
+
+    try {
+        const response = await fetch('https://api.themoviedb.org/3/person/popular?language=en-US&page=1', options)
+        const data = await response.json()
+        const originalArray = data.results
+        const filteredArray = originalArray.map(item => ({
+            profile_path: item.profile_path,
+            name: item.name,
+            known_for: item.known_for.map(item => ({
+                title: item.title,
+                original_title: item.original_title,
+                vote_average: item.vote_average
+            })),
+            popularity: item.popularity
+        }))
+        
+        return res.status(200).json(filteredArray)
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({error: "Falha ao buscar dados"})
+    }
+
 })
 
 app.post('/search', async(req, res) => {
@@ -78,10 +126,19 @@ app.get('/genre/:id', async(req, res) => {
 
     const id = req.params.id
     if(!id || isNaN(id)) return res.status(400).json({error: "Gênero do filme inválido ou não informado"})
-
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${id}`
     
-    await fetchData(url, res)
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${id}`, options)
+        const data = await response.json()
+        const originalArray = data.results
+        const filteredArray = originalArray.map(item => ({
+            backdrop_path: item.backdrop_path
+        }))
+        return res.status(200).json(filteredArray)
+    } catch(err) {
+        console.error(err)
+        return res.status(500).json({error: 'Falha ao buscar dados'})
+    }
     
 })
 
